4118ee842a61a4bc725e55f71d89306cb36d4503
WladiCosta <wladimircosta.dev@gmail.com>
2024-02-18 18:05:54 -0300

    att

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='flex items-center w-full h-full bg-cover bg-center' 
       style={{backgroundImage: "url(/main-bg.webp)"}}>
         <div className='pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]'>
-          <h1 className='text-[50px] text-white font-semibold'>
+          <h1 className='text-[45px] text-white font-semibold'>
             Você pode fazer de tudo com 
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500'>
               {" "}
